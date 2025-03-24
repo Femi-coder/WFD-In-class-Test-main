bc7519024fa3a8e4be2a1de3bf1274f505e05342
Femi-coder <B00152778@mytudublin.ie>
2025-03-24 14:32:07 +0000

    main class set up

---
+++ PartA.py
@@ -0,0 +1,8 @@
+#A2 Main Class
+class Phone:
+    def __init__(self, brand, model, year, price, colour):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.price = price
+        self.colour = colour
